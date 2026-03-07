c0fea7ffa3fa8f52a6ec8037acfde3a36ffcb560
kennewo-02 <ccchzoom@gmail.com>
2026-03-07 04:20:16 -0600

    W1D3-Assignment commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { check, validationResult} = require('express-validator');
+const router = express.Router();
+
+router.get('/', 
+    [
+    check('name')
+        .isLength({ min: 1 })
+        .withMessage('Please enter name'),
+    check('email')
+        .isLength({ min: 1 })
+        .withMessage('Please enter an email'),
+    ],
+
+    function(req, res) {
+    res.render('form', {title: 'Registration form'});
+});
+
+router.post('/', function(req, res) {
+    //console.log(req.body);
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+        res.send('Thank you for your registration!');
+    } else {
+        res.render('form', {
+            title: 'Registration form',
+            errors: errors.array(),
+            data: req.body,
+        });
+    }
+});
+
+module.exports = router;
