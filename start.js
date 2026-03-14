525f47847d950a94828e25c35689cb7e407a91bc
kennewo-02 <ccchzoom@gmail.com>
2026-03-14 10:50:33 -0500

    Defined Mongoose Schema

--- start.js
+++ start.js
@@ -1,5 +1,22 @@
+require('dotenv').config();
+require('./models/Registration');
 const app = require('./app');
+const mongoose = require('mongoose');
 
-const server = app.listen(3000, function() {
+
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => {
     console.log(`Express is running on port ${server.address().port}`);
 });
+
+// Connect to MongoDB without deprecated options
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection
+  .on('open', () => {
+      console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+      console.log(`Connection error: ${err.message}`);
+  });
